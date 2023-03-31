e5176e7afb8482992f7b9d4d68426c00598f73ba
scott <scott@boardware.com>
2023-03-31 21:55:39 +0800

    add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ module.exports = (...args) => {
     const loadRemotePreset = require('./loadRemotePreset');
     const [isGitHub, urlObj] = args;
     let url = 'direct:' + isGitHub
-    console.log(url);
     return loadRemotePreset(url, true);
 }
